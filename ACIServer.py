a4d7f6cc2961be7420edab5e5835e7a7fd86bb21
23156436 <jordanayan@live.com>
2021-10-06 21:28:17 -0500

    Update to match Horizontal_Test_Stand Progress

--- ACIServer.py
+++ ACIServer.py
@@ -63,39 +63,39 @@ class Server:
                 user = "NotAuthed"
 
 
-            if cmd["cmd"] == "get_value":
+            if cmd["cmdType"] == "get_val":
                 response = self.get_response_packet(cmd["key"], cmd["db_key"], websocket.user)
                 await websocket.send(response)
             
-            if cmd["cmd"] == "set_value":
+            if cmd["cmdType"] == "set_val":
                 newValue = self.dbs[cmd["db_key"]].set(cmd["key"], cmd["val"], websocket.user)
-                response = json.dumps({"cmd": "set_value", "msg": str(cmd["db_key"]) + "[" + str(cmd["key"]) + "] = " + str(newValue)})
+                response = json.dumps({"cmdType": "setResp", "msg": str(cmd["db_key"]) + "[" + str(cmd["key"]) + "] = " + str(newValue)})
                 await websocket.send(response)
 
-            if cmd["cmd"] == "get_index":
-                response = json.dumps({"cmd": "get_index", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].get_index(cmd["index"], websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
+            if cmd["cmdType"] == "get_index":
+                response = json.dumps({"cmdType": "get_indexResp", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].get_index(cmd["index"], websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
                 await websocket.send(response)
 
-            if cmd["cmd"] == "set_index":
-                response = json.dumps({"cmd": "set_index", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].set_index(cmd["index"], cmd["value"], websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
+            if cmd["cmdType"] == "set_index":
+                response = json.dumps({"cmdType": "set_indexResp", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].set_index(cmd["index"], cmd["value"], websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
                 await websocket.send(response)
 
-            if cmd["cmd"] == "append_list":
-                response = json.dumps({"cmd": "append_list", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].append_index(cmd["value"], websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
+            if cmd["cmdType"] == "append_index":
+                response = json.dumps({"cmdType": "app_indexResp", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].append_index(cmd["value"], websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
                 await websocket.send(response)
 
-            if cmd["cmd"] == "get_list_length":
-                response = json.dumps({"cmd": "get_list_length", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].get_len(websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
+            if cmd["cmdType"] == "get_len_index":
+                response = json.dumps({"cmdType": "get_len_indexResp", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].get_len(websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
                 await websocket.send(response)
 
-            if cmd["cmd"] == "get_recent":
+            if cmd["cmdType"] == "get_recent_index":
                 print("Client is requesting recent index")
-                response = json.dumps({"cmd": "get_recent", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].get_recent(cmd["num"], websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
+                response = json.dumps({"cmdType": "get_recent_indexResp", "msg": self.dbs[cmd["db_key"]].data[cmd["key"]].get_recent(cmd["num"], websocket.user), "key":cmd["key"], "db_key":cmd["db_key"]})
                 await websocket.send(response)
                 print("sent")
 
             
-            if cmd["cmd"] == "event":
+            if cmd["cmdType"] == "event":
                 print(cmd)
                 print(self.clients)
                 for index in self.clients:
@@ -107,21 +107,21 @@ class Server:
                         except:
                             pass
 
-            if cmd["cmd"] == "write_to_disk":
+            if cmd["cmdType"] == "wtd":
                 self.write_to_disk(cmd["db_key"])
 
-            if cmd["cmd"] == "read_from_disk":
+            if cmd["cmdType"] == "rfd":
                 self.read_from_disk(cmd["db_key"])
 
-            if (cmd["cmd"] == "create_database"):
+            if (cmd["cmdType"] == "cdb"):
                 self.dbs[cmd["db_key"]] = Database(cmd["db_key"], read=False, root_dir=self.rootDir)
                 
-            if cmd["cmd"] == "list_keys":
-                response = json.dumps({"cmd": "ldResp",
+            if cmd["cmdType"] == "list_databases":
+                response = json.dumps({"cmdType": "ldResp",
                                        "msg": json.dumps(list(self.dbs[cmd["db_key"]].data.keys()))})
                 await websocket.send(response)
 
-            if cmd["cmd"] == "g_auth":
+            if cmd["cmdType"] == "g_auth":
                 print("Starting Google Auth")
                 try:
                     token = cmd["id_token"]
@@ -154,23 +154,23 @@ class Server:
                     # Invalid token
                     pass
 
-            if cmd["cmd"] == "a_auth":
+            if cmd["cmdType"] == "a_auth":
                 a_users = self.dbs["config"].get("a_users", "backend")
                 if cmd["id"] in a_users:
                     if cmd["token"] in a_users[cmd["id"]]["tokens"]:
                         websocket.user = {"user_type":"a_user", "user_id":cmd["id"]}
-                        response = json.dumps({"cmd": "a_auth_response", "msg": "success"})
+                        response = json.dumps({"cmdType": "a_auth_response", "msg": "success"})
                         self.clients.append(ServerClient(cmd["id"], "a_user", websocket, cmd["id"]))
                         await websocket.send(response)
                     else:
-                        response = json.dumps({"cmd": "a_auth_response", "msg": "Failed, token incorrect"})
+                        response = json.dumps({"cmdType": "a_auth_response", "msg": "Failed, token incorrect"})
                         await websocket.send(response)
                 else:
-                    response = json.dumps({"cmd": "a_auth_response", "msg": "Failed, a_user not found"})
+                    response = json.dumps({"cmdType": "a_auth_response", "msg": "Failed, a_user not found"})
                     await websocket.send(response)
 
     def get_response_packet(self, key, db_key, user):
-        return json.dumps({"cmd": "getResp", "key": key, "val": self.dbs[db_key].get(key, user), "db_key": db_key})
+        return json.dumps({"cmdType": "getResp", "key": key, "val": self.dbs[db_key].get(key, user), "db_key": db_key})
 
     def write_to_disk(self, db_key):
         if db_key != "":
